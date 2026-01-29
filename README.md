0ccfc08dd8b9fbe54dd3f81d27efdae6219a8654
TranPhuThinh14 <thcs.tqt.tn@gmail.com>
2026-01-29 22:29:16 +0700

    Tuần 1: Tạo ASP.NET MVC project và cài đặt SQL Server

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# ASPNET-DK24TTC4-tranphuthinh-Project
+# ASPNET-DK24TTC4-tranphuthinh-Webbancoffee
+
+# CoffeeShop ASP.NET MVC
+
+## Thông tin sinh viên
+- Họ tên: Trần Phú Thịnh
+- Lớp: DK24TTC4
+- Môn học: ASP.NET
+- Giảng viên hướng dẫn: TS. Đoàn Phước Miền
+
+## Mô tả đồ án
+Xây dựng website bán cà phê sử dụng ASP.NET Core MVC và SQL Server.
+Hệ thống cho phép:
+- Quản lý sản phẩm cà phê
+- Đăng nhập / đăng ký người dùng
+- Đặt hàng và quản lý đơn hàng
+
+## Công nghệ sử dụng
+- ASP.NET Core MVC
+- SQL Server
+- Entity Framework Core
+- HTML, CSS, Bootstrap
+
+## Tiến độ thực hiện
+- Tuần 1: Cài đặt môi trường, tạo database, tạo project ASP.NET MVC
+- Tuần 2: Thiết kế database, chức năng đăng nhập
+- Tuần 3: Quản lý sản phẩm
+- Tuần 4: Đặt hàng
+- Tuần 5: Hoàn thiện & báo cáo
+
+## Hướng dẫn chạy dự án
+1. Clone repository
+2. Mở bằng Visual Studio 2022
+3. Cấu hình connection string
+4. Run project
---
+++ src/Coffee/Coffee/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Coffee")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("BAN QUYEN 21AK22.COM")]
+[assembly: AssemblyProduct("Coffee")]
+[assembly: AssemblyCopyright("Copyright © BAN QUYEN 21AK22.COM 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4fc419f8-5bd1-4f9f-863a-7e61571c71e9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
